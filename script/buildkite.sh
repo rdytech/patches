4375ca294b9648c9e152d44634d03d502eb0d15a
John D'Agostino <johnd@jobready.com.au>
2015-06-16 13:14:22 +1000

    add build script

---
+++ script/buildkite.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo '--- setting ruby version'
+rbenv local 2.1.5
+
+echo '--- setting up env'
+REVISION=https://github.com/$BUILDBOX_PROJECT_SLUG/commit/$BUILDBOX_COMMIT
+
+echo '--- bundling'
+bundle install -j $(nproc) --without production --quiet
+
+echo '--- running specs'
+if bundle exec rspec; then
+  echo "[Successful] $BUILDBOX_PROJECT_SLUG - Build - $BUILDBOX_BUILD_URL - Commit - $REVISION" | hipchat_room_message -t $HIPCHAT_TOKEN -r $HIPCHAT_ROOM -f "Buildbox" -c "green"
+else
+  echo "[Failed] Build $BUILDBOX_PROJECT_SLUG - Build - $BUILDBOX_BUILD_URL - Commit - $REVISION" | hipchat_room_message -t $HIPCHAT_TOKEN -r $HIPCHAT_ROOM -f "Buildbox" -c "red"
+  exit 1;
+fi
