ac350e4891039c50d7e22b7c4fc9c42832512a1d
Emerson Xavier <msxavii@gmail.com>
2019-11-20 09:09:04 +1100

    [NEP-9115] replaces switch! with a block

--- lib/patches/tenant_run_concern.rb
+++ lib/patches/tenant_run_concern.rb
@@ -1,8 +1,9 @@
 module Patches
   module TenantRunConcern
     def run(tenant_name, path = nil)
-      Apartment::Tenant.switch!(tenant_name)
-      Patches::Runner.new(path).perform
+      Apartment::Tenant.switch(tenant_name) do
+        Patches::Runner.new(path).perform
+      end
     end
   end
 end
